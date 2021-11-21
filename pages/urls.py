8c98529064b7e0e1eab8e8c32da86da21464196f
Emin Asgarov <asgarov.emin@gmail.com>
2021-11-21 19:27:14 +0400

    created pages and configed navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+import pages
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
